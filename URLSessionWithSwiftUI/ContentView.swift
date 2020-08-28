beca7f535ad0e348fb616dc592f0e64c5530df92
ibalborde <maximiliano_009@hotmail.com>
2020-08-28 09:12:42 -0300

    Initial Commit

---
+++ URLSessionWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URLSessionWithSwiftUI
+//
+//  Created by Maximiliano Ibalborde on 28/08/2020.
+//  Copyright © 2020 MaxiIbalborde. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
